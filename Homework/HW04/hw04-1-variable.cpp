73eb7a417595b042589560cbf0a217145f8f556e
your name <fifasisa36@gmail.com>
2023-08-11 16:05:24 +0700

    hw

--- Homework/HW04/hw04-1-variable.cpp
+++ Homework/HW04/hw04-1-variable.cpp
@@ -4,9 +4,15 @@
     Output:
         10 20.0 Haha a
 */
+#include <stdio.h>
+    int main(){
+    int a = 10 ;
+    float b = 20.0 ;
+    char c []= "Haha";
+    char d []= "a";
+    printf("_ _ _ _\n") ; //TODO: #10 Display variable a, b, c and d.
+    scanf("%d",a ,b ,c ,d);
+    printf("10 20.0 Haha a\n",a ,b ,c ,d);
+    return 0; }
 
-int a = 10 ;
-float b = 20 ;
-char c[] = "Haha" ;
-char d = 'a' ;
-printf( "_ _ _ _", _, _, _, _) ; //TODO: #10 Display variable a, b, c and d.
+    
