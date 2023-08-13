5be186ede28c2a5c49e9e79cb9fb88c737f3df6d
your name <fifasisa36@gmail.com>
2023-08-13 11:05:16 +0700

    hw4-1

--- Homework/HW04/hw04-1-variable.cpp
+++ Homework/HW04/hw04-1-variable.cpp
@@ -11,7 +11,7 @@
     char c []= "Haha";
     char d []= "a";
     printf("input:\n");
-    scanf("%d %d %d %d",&a ,&b ,&c ,&d); //TODO: #10 Display variable a, b, c and d.
+    scanf("%d %f %s %s",&a ,&b ,&c ,&d); //TODO: #10 Display variable a, b, c and d.
     printf("Output:\n %d %0.1f %s %s",a ,b ,c ,d);
     return 0; }
 
